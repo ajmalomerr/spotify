d485f8d83eb07a23307be5b00d7472ae3d85d9e3
ajmal.kp <ajmal.kp@marensolutions.com>
2023-12-05 12:50:48 +0530

    search query added

--- src/screens/HomeScreen/index.tsx
+++ src/screens/HomeScreen/index.tsx
@@ -1,8 +1,52 @@
-import { FlatList, View } from 'react-native';
+import { ActivityIndicator, FlatList, StyleSheet, Text, View } from 'react-native';
 import TrackListItem from '../../Component/TrackListItem';
-import { tracks } from '../../../assets/data/tracks';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+  query MyQuery($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            url
+            height
+            width
+          }
+        }
+      }
+    }
+  }
+`
 
 const HomeScreen = () => {
+
+  const { data, loading, error } = useQuery(query, {
+    variables: { genres: 'pop' },
+  });
+
+  if (loading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      </View>
+    );
+  }
+
+  if (error) {
+    return <Text style={{ color: "#fff" }}>Failed to fetch recommendations. {error.message}</Text>;
+  }
+
+  const tracks = data?.recommendations?.tracks || [];
+
   return (
     <View style={{ backgroundColor: "black" }}>
       <FlatList
@@ -14,3 +58,12 @@ const HomeScreen = () => {
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "black",
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
--- src/screens/SearchScreen/index.tsx
+++ src/screens/SearchScreen/index.tsx
@@ -1,51 +1,89 @@
-import { FlatList, SafeAreaView, View, StyleSheet, TextInput, Text } from 'react-native';
+import { FlatList, SafeAreaView, View, StyleSheet, TextInput, Text, ActivityIndicator } from 'react-native';
 import TrackListItem from '../../Component/TrackListItem';
-import { tracks } from '../../../assets/data/tracks';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import { useState } from 'react';
+import { gql, useQuery } from '@apollo/client';
+
+const query = gql`
+query MyQuery($q: String!) {
+    search(q: $q) {
+      tracks {
+        items {
+          id
+          name
+          preview_url
+          artists {
+            id
+            name
+          }
+          album {
+            id
+            name
+            images {
+              url
+              height
+              width
+            }
+          }
+        }
+      }
+    }
+  }`
 
 const SearchScreen = () => {
-    const [search, setSearch] = useState('');
+  const [search, setSearch] = useState('');
 
-    return (
-        <SafeAreaView style={styles.container}>
-            <View style={styles.header}>
-                <FontAwesome name="search" size={16} color="gray" />
-                <TextInput
-                    value={search}
-                    placeholderTextColor={"grey"}
-                    placeholder="What do you want to listen to?"
-                    onChangeText={setSearch}
-                    style={styles.input}
-                />
-                <Text onPress={() => setSearch('')} style={{ color: "#fff" }}>Cancel</Text>
-            </View>
+  const { data, loading, error } = useQuery(query, { variables: { q: search } });
 
-            <FlatList
-                data={tracks}
-                renderItem={({ item }) => <TrackListItem track={item} />}
-            />
-        </SafeAreaView>
-    );
+  const tracks = data?.search?.tracks?.items || []
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header}>
+        <FontAwesome name="search" size={16} color="gray" />
+        <TextInput
+          value={search}
+          placeholderTextColor={"grey"}
+          placeholder="What do you want to listen to?"
+          onChangeText={setSearch}
+          style={styles.input}
+        />
+        <Text onPress={() => setSearch('')} style={{ color: "#fff" }}>Cancel</Text>
+      </View>
+
+      {loading && <View style={{}}>
+        <ActivityIndicator color={"#fff"} size={"large"} />
+      </View>}
+
+      {/* {error && <Text style={{ color: "#fff" }}>
+        Failed to fetch recommendations. {error.message}
+      </Text>} */}
+
+      <FlatList
+        data={tracks}
+        renderItem={({ item }) => <TrackListItem track={item} />}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "black"
-    },
-    header: {
-        flexDirection: 'row',
-        alignItems: 'center',
-    },
-    input: {
-        backgroundColor: '#121314',
-        color: 'white',
-        flex: 1,
-        marginHorizontal: 10,
-        padding: 8,
-        borderRadius: 5,
-    },
+  container: {
+    flex: 1,
+    backgroundColor: "black"
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    backgroundColor: '#121314',
+    color: 'white',
+    flex: 1,
+    marginHorizontal: 10,
+    padding: 8,
+    borderRadius: 5,
+  },
 });
 
 export default SearchScreen
